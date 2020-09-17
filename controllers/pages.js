45f7d1c47808bd9afe884405059303c91fe2df6d
Asim Zaidi <azaidi@livly.io>
2020-09-17 01:20:12 -0500

    faq wip

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,56 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('home');
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
-router.get('/contact', (req, res) => {
-  res.render('contact');
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
-router.get('/facility', (req, res) => {
-  res.render('about/facility');
+router.get("/facility", (req, res) => {
+  res.render("about/facility");
 });
 
-router.get('/mission', (req, res) => {
-  res.render('about/mission');
+router.get("/mission", (req, res) => {
+  res.render("about/mission");
 });
 
-router.get('/team', (req, res) => {
-  res.render('about/team');
+router.get("/faq", (req, res) => {
+  res.render("about/faq");
 });
 
-router.get('/bootcamp', (req, res) => {
-  res.render('services/bootcamp');
+router.get("/team", (req, res) => {
+  res.render("about/team");
 });
 
-router.get('/aggressioncamp', (req, res) => {
-  res.render('services/aggressioncamp');
+router.get("/bootcamp", (req, res) => {
+  res.render("services/bootcamp");
 });
 
-router.get('/privatelesson', (req, res) => {
-  res.render('services/privatelesson');
+router.get("/aggressioncamp", (req, res) => {
+  res.render("services/aggressioncamp");
 });
 
-router.get('/privatelessonbundle', (req, res) => {
-  res.render('services/privatelessonbundle');
+router.get("/privatelesson", (req, res) => {
+  res.render("services/privatelesson");
 });
 
-router.get('/servicedogtraining', (req, res) => {
-  res.render('services/servicedogtraining');
+router.get("/privatelessonbundle", (req, res) => {
+  res.render("services/privatelessonbundle");
 });
 
-router.get('/therapydogtraining', (req, res) => {
-  res.render('services/therapydogtraining');
+router.get("/servicedogtraining", (req, res) => {
+  res.render("services/servicedogtraining");
 });
 
-router.get('/protectiondogtraining', (req, res) => {
-  res.render('services/protectiondogtraining');
+router.get("/therapydogtraining", (req, res) => {
+  res.render("services/therapydogtraining");
 });
 
-router.get('/boarding', (req, res) => {
-  res.render('boarding');
+router.get("/protectiondogtraining", (req, res) => {
+  res.render("services/protectiondogtraining");
+});
+
+router.get("/boarding", (req, res) => {
+  res.render("boarding");
 });
 
 module.exports = router;
